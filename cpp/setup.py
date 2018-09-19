fa294c35a76c4b402fb1a791a79ae17a9f284d84
Jonathan Zheng <jz@jonatharbookpro.lan>
2018-09-19 12:15:48 +0100

    tidied up python package

--- cpp/setup.py
+++ cpp/setup.py
@@ -18,7 +18,7 @@ _layout = Extension(
 
 setup(
     name="s_gd2",
-    version="0.13",
+    version="0.14",
     author="Jonathan Zheng",
     author_email="jxz12@ic.ac.uk",
     description="A package for performing stochastic gradient descent (arXiv:1710.04626) to layout graphs",
