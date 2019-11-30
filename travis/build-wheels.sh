122c4e32bf350447fd205f05056efcf9ba9921a0
Scott Gigante <scottgigante@gmail.com>
2019-11-30 10:16:21 -0500

    install nose2

--- travis/build-wheels.sh
+++ travis/build-wheels.sh
@@ -32,6 +32,7 @@ for PYBIN in /opt/python/*/bin; do
     # Install and test
     "${PYBIN}/pip" install s_gd2 --no-index -f "${REPAIR_DIR}"
     cd /io/cpp/
+    "${PYBIN}/pip" install .[test]
     "${PYBIN}/python" setup.py test
     cd ..
 
