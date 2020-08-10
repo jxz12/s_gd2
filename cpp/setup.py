c5e45061d8a0df4443b9db3d0d853ce4bb34d247
Scott Gigante <scottgigante@users.noreply.github.com>
2020-08-10 13:49:09 -0400

    Lazy load numpy in setup.py

--- cpp/setup.py
+++ cpp/setup.py
@@ -4,12 +4,20 @@ from setuptools.extension import Extension
 import os
 
 # Third-party modules - we depend on numpy for everything
-import numpy
-# Obtain the numpy include directory.  This logic works across numpy versions.
-try:
-    numpy_include = numpy.get_include()
-except AttributeError:
-    numpy_include = numpy.get_numpy_include()
+class get_numpy_include(object):
+    """Returns Numpy's include path with lazy import.
+    """
+    def __str__(self):
+        import numpy
+        # Obtain the numpy include directory.  This logic works across numpy versions.
+        try:
+            numpy_include = numpy.get_include()
+        except AttributeError:
+            numpy_include = numpy.get_numpy_include()
+
+setup_requires = [
+    'numpy>=1.16',
+]
 
 install_requires = [
     'numpy>=1.16',
@@ -24,7 +32,7 @@ _layout = Extension(
     name="_layout",
     headers=["./s_gd2/layout.hpp", "./s_gd2/randomkit.h"],
     sources=["./s_gd2/layout.cpp", "./s_gd2/sparse.cpp", "./s_gd2/swig/layout_wrap.cxx", "./s_gd2/randomkit.c"],
-    include_dirs=[numpy_include],
+    include_dirs=[get_numpy_include()],
 )
 
 version_py = os.path.join(os.path.dirname(__file__), 's_gd2', 'version.py')
@@ -37,6 +45,7 @@ setup(
     author_email="jxz12@ic.ac.uk",
     url="https://www.github.com/jxz12/s_gd2",
     description="A package for performing stochastic gradient descent (arXiv:1710.04626) to layout graphs",
+    setup_requires=setup_requires,
     install_requires=install_requires,
     extras_require={'test':test_requires},
     test_suite='nose2.collector.collector',
